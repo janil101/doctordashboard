b69591d6e38be08ea93d4dea5ccf605955675914
janildattani <dattanijanil3@gmail.com>
2024-10-23 21:33:25 +0530

    initial commit

---
+++ src/main/java/com/example/doctordashboard/DoctordashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.doctordashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctordashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctordashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doctordashboard/DoctordashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctordashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctordashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
